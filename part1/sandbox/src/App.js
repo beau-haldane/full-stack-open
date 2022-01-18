0376d2fdd57dbcb5d8e4a81c3257ecbebfba98e8
beau-haldane <beauhaldane@gmail.com>
2022-01-18 11:10:16 +1000

    working through part 1c, beginning to understand useState

--- part1/sandbox/src/App.js
+++ part1/sandbox/src/App.js
@@ -2,9 +2,32 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-  setTimeout(    () => setCounter(counter + 1),    1000  )
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
+    </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
 
