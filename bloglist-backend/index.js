32876b06d8d00d51fd0e8f19a3e052d404990caf
beau-haldane <beauhaldane@gmail.com>
2022-03-15 13:33:32 +1000

    Setting up backend for upcoming bloglist app

---
+++ bloglist-backend/index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const app = require('./app')
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+
+const server = http.createServer(app)
+
+
+
+
+// -- Run Server -- //
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
---
+++ bloglist-backend/models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+// Set schema for blogs
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+// Format object returned by MongoDB
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+// Export module and return Mongo model
+module.exports = mongoose.model('Blog', blogSchema)
